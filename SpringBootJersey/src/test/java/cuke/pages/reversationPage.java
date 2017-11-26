ff36aaf318101e1195691bbd9af55bf005794dfe
LeBron-si <499842961@qq.com>
2017-11-26 17:14:22 +0800

    team-b commit

---
+++ SpringBootJersey/src/test/java/cuke/pages/reversationPage.java
@@ -0,0 +1,39 @@
+package cuke.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by Karthik on 10/25/2016.
+ */
+public class reversationPage {
+
+    public reversationPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(how = How.NAME, using = "FromTime")
+    public WebElement txtFromTime;
+
+    @FindBy(how = How.NAME, using = "EndTime")
+    public WebElement txtEndTime;
+
+    @FindBy(how = How.NAME, using = "Rev")
+    public WebElement btnRev;
+
+    public void Reversation(String FromTime, String EndTime)
+    {
+        txtFromTime.sendKeys(FromTime);
+        txtEndTime.sendKeys(EndTime);
+    }
+
+    public void ClickRev()
+    {
+        btnRev.submit();
+    }
+
+
+}
---
+++ SpringBootJersey/src/test/java/cuke/steps/reversationSteps.java
@@ -0,0 +1,32 @@
+package cuke.steps;
+
+import cucumber.api.PendingException;
+
+/**
+ * Created by Lenovo on 2017/11/26.
+ */
+public class reversationSteps {
+    @cucumber.api.java.zh_cn.那么("^我预定失败$")
+    public void 我预定失败() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @cucumber.api.java.zh_cn.当("^我预定崇文门场馆(\\d+)点到(\\d+)点的一个场地$")
+    public void 我预定崇文门场馆_点到_点的一个场地(int arg1, int arg2) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @cucumber.api.java.zh_cn.假设("^在崇文门场馆，(\\d+)点到(\\d+)点有空余场地$")
+    public void 在崇文门场馆_点到_点有空余场地(int arg1, int arg2) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @cucumber.api.java.zh_cn.那么("^我能成功预定$")
+    public void 我能成功预定() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+}
